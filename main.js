86e99a1379ea7bf7f82309c8c371f17e49ead8c0
Jesid <nelsonpiloto29@gmail.com>
2023-04-21 07:46:51 -0500

    rama 2

--- main.js
+++ main.js
@@ -1,40 +1,72 @@
 const display = document.getElementById('display')
 const num1 = document.getElementById('num1')
-const num2 = document.getElementById('num2')
 const operando = document.getElementById('operando')
 const boton = document.querySelector('.grid-container')
+const limpiar = document.getElementById('limpiar')
+const igual = document.getElementById('igual')
 
+window.addEventListener('DOMContentLoaded', ()=>{
+    display.value=''
+})
+
+let guardar;
 boton.addEventListener('click',(e)=>{
-    e.target.id=='suma' ? operando.textContent="+":  e.target.id=='rest' ? operando.textContent="-" :  e.target.id=='multi' ? operando.textContent="*": e.target.id=='divi' ? operando.textContent="/":"" 
     let num1Parse = parseFloat(num1.value)
-    let num2Parse = parseFloat(num2.value)
-    if(isNaN(num1Parse) || isNaN(num2Parse) || operando.textContent==''){
+    if(isNaN(num1Parse)){
         return
     }
-   if(e.target.id==='igual'){
-        switch (operando.textContent) {
-            case "+":  
-            display.value=num1Parse+num2Parse;
-            break;
-            case "-":
-            display.value=num1Parse-num2Parse;
-            break;
-            case "*":
-            display.value=num1Parse*num2Parse;
-            break;
-            case "/":
-            display.value=num1Parse/num2Parse;
-            break;
-            default:
-            console.log('¡Ups! tenemos un Error');
-            break;
-        } 
+    if(e.target.id=='suma'){
+        operando.textContent="+" 
+        guardar = num1Parse
+        num1.value=''
+    }else if(e.target.id=='rest'){
+        operando.textContent="-"
+        guardar = num1Parse
+        num1.value=''
+    }else if(e.target.id=='multi'){
+        operando.textContent="*"
+        guardar = num1Parse
+        num1.value=''
+    }else if(e.target.id=='divi'){
+        operando.textContent="/"
+        guardar = num1Parse
+        num1.value=''
+    }else{
+        return
+    }
+})
 
-   }
-   if(e.target.id==='limpiar'){
-        display.value='';
+igual.addEventListener('click', ()=>{
+    let  num2Parse = parseFloat(num1.value)
+    switch (operando.textContent) {
+        case "+":  
+        isNaN(num2Parse) ? display.value=guardar :  display.value=guardar+num2Parse;
+        guardar = 0;
         num1.value='';
-        num2.value='';
-        operando.textContent='';
-   }
+        break;
+        case "-":
+        isNaN(num2Parse) ? display.value=guardar :  display.value=guardar-num2Parse;
+        guardar = 0;
+        num1.value='';
+        break;
+        case "*":
+        isNaN(num2Parse) ? display.value=guardar :  display.value=guardar*num2Parse;
+        guardar = 0;
+        num1.value='';  
+        break;
+        case "/":
+        isNaN(num2Parse) ? display.value=guardar :  display.value=guardar/num2Parse;
+        guardar = 0;
+        num1.value='';  
+        break;
+        default:
+        console.log('¡Ups! tenemos un Error');
+        break;
+    } 
+})
+
+limpiar.addEventListener('click', ()=>{
+    display.value='';
+    num1.value='';
+    operando.textContent='';
 })
